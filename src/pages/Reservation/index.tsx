c2ed9e39a4a8cf5dbf1a2be7b2bacdf25599a8b3
DaoYuan <txp1035@gmail.com>
2025-10-08 17:57:44 +0800

    feat: 预约页面增加tab

--- src/pages/Reservation/index.tsx
+++ src/pages/Reservation/index.tsx
@@ -1,4 +1,4 @@
-import { deleteInfo, editInfo, getInfo } from '@/utils/leancloud';
+import { editInfo, getInfo } from '@/utils/leancloud';
 import type { ActionType } from '@ant-design/pro-components';
 import {
   PageContainer,
@@ -6,7 +6,8 @@ import {
   ProTable,
 } from '@ant-design/pro-components';
 import { Button, Modal } from 'antd';
-import React, { useRef } from 'react';
+import dayjs from 'dayjs';
+import React, { useRef, useState } from 'react';
 import Form from './Form';
 
 const isDev = false;
@@ -53,38 +54,77 @@ function common(columns) {
   };
 }
 
+function getIsEdit(tabList) {
+  const 备战时间 = '20251006';
+  const 当前时间 = +new Date();
+  let isEdit = false;
+  switch (tabList) {
+    case '1':
+      const 研究加速填写截止时间 =
+        dayjs(备战时间).subtract(1, 'day').format('YYYYMMDD') + '20';
+      if (+dayjs(研究加速填写截止时间) > 当前时间) {
+        isEdit = true;
+      }
+      break;
+    case '2':
+      const 建筑加速填写截止时间 =
+        dayjs(备战时间).add(3, 'day').format('YYYYMMDD') + '20';
+      if (+dayjs(建筑加速填写截止时间) > 当前时间) {
+        isEdit = true;
+      }
+      break;
+    case '3':
+      const 练兵加速填写截止时间 =
+        dayjs(备战时间).add(2, 'day').format('YYYYMMDD') + '20';
+      if (+dayjs(练兵加速填写截止时间) > 当前时间) {
+        isEdit = true;
+      }
+      break;
+    default:
+      break;
+  }
+  return isEdit;
+}
+
 const TableList: React.FC<unknown> = () => {
+  const [tabList, setTabList] = useState('1');
+  const isEdit = getIsEdit(tabList);
   const actionRef = useRef<ActionType>();
+  const option = isEdit
+    ? [
+        {
+          title: '操作',
+          valueType: 'option',
+          key: 'option',
+          width: 200,
+          render: (text, record, _, action) => [
+            <Form type="edit" actionRef={actionRef} info={record} />,
+            <a
+              key="view"
+              onClick={() => {
+                Modal.confirm({
+                  title: '是否删除',
+                  content: `${record.name}的预约`,
+                  onOk: async () => {
+                    await deleteInfo(record.id);
+                    actionRef.current?.reload();
+                  },
+                });
+              }}
+            >
+              删除
+            </a>,
+          ],
+        },
+      ]
+    : [];
   const columns: ProDescriptionsItemProps<API.UserInfo>[] = [
     {
       title: '顺序',
       valueType: 'index',
-      width: 50,
+      width: 60,
     },
-    {
-      title: '操作',
-      valueType: 'option',
-      key: 'option',
-      width: 200,
-      render: (text, record, _, action) => [
-        <Form type="edit" actionRef={actionRef} info={record} />,
-        <a
-          key="view"
-          onClick={() => {
-            Modal.confirm({
-              title: '是否删除',
-              content: `${record.name}的预约`,
-              onOk: async () => {
-                await deleteInfo(record.id);
-                actionRef.current?.reload();
-              },
-            });
-          }}
-        >
-          删除
-        </a>,
-      ],
-    },
+    ...option,
     {
       title: '游戏名',
       dataIndex: 'name',
@@ -110,7 +150,7 @@ const TableList: React.FC<unknown> = () => {
     },
     {
       title: '实际时间',
-      width: 90,
+      width: 100,
       dataIndex: 'actualTime',
       render: (_, record) => {
         return record.actualTime === -1 ? '没有匹配' : record.actualTime;
@@ -139,6 +179,18 @@ const TableList: React.FC<unknown> = () => {
 
   return (
     <PageContainer
+      tabList={[
+        { tab: '建筑加速（周一）', key: '1' },
+        { tab: '练兵加速（周四）', key: '3' },
+        { tab: '研究加速（周五）', key: '2' },
+      ]}
+      tabActiveKey={tabList}
+      tabProps={{
+        onChange: (value) => {
+          setTabList(value);
+          actionRef.current?.reload();
+        },
+      }}
       header={{
         title: '官职预约',
       }}
@@ -154,12 +206,10 @@ const TableList: React.FC<unknown> = () => {
               type="primary"
               onClick={async () => {
                 const res = await getInfo();
-                console.log(res);
                 const data = res.sort((a, b) => b.number - a.number);
                 const newData = calculateTime(
-                  data.filter((item) => item.type === 2),
+                  data.filter((item) => item.type === Number(tabList)),
                 );
-
                 for (let index = 0; index < newData.length; index++) {
                   const element = newData[index];
                   await editInfo({
@@ -170,18 +220,17 @@ const TableList: React.FC<unknown> = () => {
                 Modal.info({ title: '更新成功' });
               }}
             >
-              生成建筑预约时间
+              生成预约时间
             </Button>
           ),
         ]}
         request={async (params, sort, filter) => {
           const res = await getInfo();
-          console.log(res);
           const data = res.sort((a, b) => b.number - a.number);
           const newData = calculateTime(
-            data.filter((item) => item.type === 2),
+            data.filter((item) => item.type === Number(tabList)),
           ).sort((a, b) => a.actualTime - b.actualTime);
-          return { data: data };
+          return { data: newData };
         }}
         columns={columns}
         {...common(columns)}
