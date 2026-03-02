7015df2cc78760f57551be51fbafd862667f4f61
DaoYuan <txp1035@gmail.com>
2026-03-02 19:21:31 +0800

    chore: 国战数据

--- src/pages/521/data/index.ts
+++ src/pages/521/data/index.ts
@@ -1,9 +1,11 @@
 import { IS_DEV } from '@/constants';
 import 国战20260201 from './20260201国战.json';
+import 国战20260301 from './20260301国战.json';
 import data from './data.json';
 
 const obj = IS_DEV ? { ...data } : {};
 export default {
   ...obj,
   '2026-02-01国战': 国战20260201,
+  '2026-03-01国战': 国战20260301,
 };
